20f6835fe78fea9eeec208c97e1f169cb30ec0ab
Quinten van Breugel <qvbreugel@gmail.com>
2020-10-14 23:35:44 +0200

    Redid code without gyroscope

--- LIT.ino
+++ LIT.ino
@@ -1,80 +1,44 @@
-//Nodig om waarde van de accelerometer en gyroscope te lezen
-#include <Wire.h>
-
-//Microprocessor van accelerometer/gyroscope
-const int MPU = 0x68;
-
-//Initialiseer op te nemen waarden
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-
-// Aan/uit knop
+// Pin of connected power button
 int powerButton = 8;
 
-// Circuit break om geen stroom door te laten als het apparaat uit staat
-int circuitBreak = 5;
+// Pin of the solenoid valve
+int solenoidValve = 4;
+
+//Pin of the connected LED
+int LED = 5;
+
 
 void setup()
 {
     pinMode(powerButton, INPUT);   //powerButton is input
+    pinMode(solenoidValve, INPUT); //solenoid valve is input
     pinMode(LED, OUTPUT);          //LED is output
-    pinMode(circuitBreak, OUTPUT); //Circuit break is input
-
-    //Koppeling van accelerometer/gyroscope
-    Wire.begin();
-    Wire.beginTransmission(MPU);
-    Wire.write(0x6B);
-    Wire.write(0);
-    Wire.endTransmission(true);
-    Serial.begin(9600);
 }
 
 void loop()
 {
-    //Houdt de huidige staat van de aan/uit knop bij
+    //Keep track of the current state of the button
     int stateOfButton = digitalRead(powerButton);
-    // Checken of de powerbutton is ingedrukt of niet
+    
+    // Check if button is not pressed
     if (stateOfButton == 0)
     {
-        //Als de knop uitstaat, zorg dat er geen stroom door het circuit gaat
-        digitalWrite(circuitBreak, LOW);
-        //En zet de LED uit
+        //Turn LED off if the button is not pressed
         digitalWrite(LED, LOW);
+
+        //Close the valve if the button is not pressed
+        digitalWrite(solenoidValve, LOW);
     }
 
+    // Check if button is pressed
     if (stateOfButton == 1)
     {
-        //Als de knop aanstaat, zorg dat er wel stroom door het circuit gaat
-        digitalWrite(circuitBreak, HIGH);
-        //En zet de LED aan
-        digitalWrite(LED, HIGH);
+        //Turn the LED on when button is pressed
+         digitalWrite(LED, HIGH);
+    
+        //Open valve when button is pressed
+        digitalWrite(solenoidValve, HIGH);
+       
 
-        //Lees de waarden van accelerometer/gyroscope
-        Wire.beginTransmission(MPU);
-        Wire.write(0x3B);
-        Wire.endTransmission(false);
-        Wire.requestFrom(MPU, 12, true);
-        AcX = Wire.read() << 8 | Wire.read();
-        AcY = Wire.read() << 8 | Wire.read();
-        AcZ = Wire.read() << 8 | Wire.read();
-        GyX = Wire.read() << 8 | Wire.read();
-        GyY = Wire.read() << 8 | Wire.read();
-        GyZ = Wire.read() << 8 | Wire.read();
-
-        Serial.print("Accelerometer: ");
-        Serial.print("X = ");
-        Serial.print(AcX);
-        Serial.print(" | Y = ");
-        Serial.print(AcY);
-        Serial.print(" | Z = ");
-        Serial.println(AcZ);
-
-        Serial.print("Gyroscope: ");
-        Serial.print("X = ");
-        Serial.print(GyX);
-        Serial.print(" | Y = ");
-        Serial.print(GyY);
-        Serial.print(" | Z = ");
-        Serial.println(GyZ);
-        Serial.println(" ");
     }
 }
